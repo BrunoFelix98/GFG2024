18302c42c2f39d2e6b6ef864301ff0e15eb0918a
John <accounts.8mr28@aleeas.com>
2024-04-06 17:30:05 +0100

    Main Menu + UI Scene

--- Assets/Code/Systems/UI/UI.cs
+++ Assets/Code/Systems/UI/UI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
@@ -14,5 +15,15 @@ public class UI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
